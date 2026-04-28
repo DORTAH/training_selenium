a949d40f00e8aa4721cb86342160ba97e2a0b939
Maksim Levin <sim.lev03@gmail.com>
2026-04-28 12:42:51 +0300

    uploud_in_taiga

---
+++ Should_be/pages/Product_page.py
@@ -0,0 +1,7 @@
+from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_product_to_cart(self):
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
+        add_button.click()
